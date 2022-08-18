8927db446e9d156bd2eb26137af6d1c2bc82a9c4
nykolas1986 <nyko858@gmail.com>
2022-08-18 15:45:17 -0700

    9

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -66,12 +66,33 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+ switch (color ) {
+  case "blue":
+  return ("This is blue")
+  case "red":
+  return ( "This is red")
+  case "green":
+  return ("This is green")
+  case "orange":
+  return ("This is orange")
+  default:
+  return ("Color not found")
+  } 
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch (numero) {
+   case 10:
+   return (true)
+   case 5:
+   return (true)
+   default:
+  return (false)
+  }
+
 }
 
 function estaEnRango(numero) {
